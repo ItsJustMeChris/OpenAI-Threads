56ad547c32e32b4687fd7a03dc00d60c01b99989
Chris McGravey <chris@mcgravey.dev>
2023-11-07 11:07:24 -0600

    Update example with functions / instructions used.

--- example.mjs
+++ example.mjs
@@ -47,3 +47,32 @@ await thread.createUserMessage(
   "How many users were at the event on 2023-10-10."
 );
 await thread.runThread("Help the user.");
+
+/*
+
+Assistant Functions:
+
+{
+  "name": "get_event_patrons_count_on_date",
+  "description": "Get the number of patrons at an event on a specific date",
+  "parameters": {
+    "type": "object",
+    "properties": {
+      "date": {
+        "type": "string",
+        "description": "The date of the event"
+      }
+    },
+    "required": [
+      "date"
+    ]
+  }
+}
+
+------
+
+Assistant Instructions:
+
+You're an event planner's assistant, you will help them monitor, run, and otherwise handle events to the best of your ability!
+
+*/
